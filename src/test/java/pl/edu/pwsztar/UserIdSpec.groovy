0773fe89d680f2e95378c0028b39288f7554796b
Michał Boruch <miboruch@gmail.com>
2020-06-01 09:29:14 +0200

    Added UserId tests

--- src/test/java/pl/edu/pwsztar/UserIdSpec.groovy
+++ src/test/java/pl/edu/pwsztar/UserIdSpec.groovy
@@ -3,9 +3,38 @@ package pl.edu.pwsztar
 import spock.lang.Specification
 
 class UserIdSpec extends Specification {
+    /* correct, incorrect, empty, null */
 
     def "should check correct size"() {
-        expect:
-            true
+        given:
+            UserId userId = new UserId("98051101192")
+        when:
+            boolean isCorrect = userId.isCorrect();
+        then:
+            isCorrect
+    }
+    def "should check incorrect size"() {
+        given:
+            UserId userId = new UserId("980511011921")
+        when:
+            boolean isCorrect = userId.isCorrect();
+        then:
+            !isCorrect
+    }
+    def "should check empty string"() {
+        given:
+            UserId userId = new UserId("")
+        when:
+            boolean isCorrect = userId.isCorrect();
+        then:
+            !isCorrect
+    }
+    def "should check null"() {
+        given:
+            UserId userId = new UserId(null)
+        when:
+            boolean isCorrect = userId.isCorrect();
+        then:
+            !isCorrect
     }
 }
